41b8f50da0f5099b610f2d81108f4bc1c06a045a
Kaleb Armbrust <armbrustcopyco@gmail.com>
2020-10-06 08:02:01 -0700

    Finished basic route setup + tickets 28/22/21 --under review

--- sprint-1/client/src/App.js
+++ sprint-1/client/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
     <h1>InStock</h1>
+    <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={() => <route />} />
+          <Route path="/" exact component={() => <route />} />
+          <Route path="/" exact component={() => <route />} />
+          <Route path="/" exact component={() => <route />} />
+          <Route path="/" exact component={() => <route />} />
+          <Route path="/" exact component={() => <route />} />
+          <Route path="/" exact component={() => <route />} />
+          <Route path="/" exact component={() => <route />} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
