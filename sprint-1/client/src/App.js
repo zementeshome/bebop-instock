f7e891bba9b5f619998da1128cd9046178806fcd
Kaleb Armbrust <armbrustcopyco@gmail.com>
2020-10-10 23:31:42 -0700

    accepted changes app.js

--- sprint-1/client/src/App.js
+++ sprint-1/client/src/App.js
@@ -12,7 +12,6 @@ import EditInventoryItem from "./Components/EditInventoryItem/EditInventoryItem"
 import AddNewInventoryItem from "./Components/AddNewInventoryItem/AddNewInventoryItem";
 import DeleteInventory from "./Components/DeleteInventory/DeleteInventory";
 import DeleteWarehouse from "./Components/DeleteWarehouse/DeleteWarehouse";
-<<<<<<< HEAD
 import Header from "./Components/Header/Header"
 
 class App extends React.Component {
@@ -29,25 +28,6 @@ class App extends React.Component {
 // };
 
   render () {
-=======
-import Header from "./Components/Header/Header";
-import axios from "axios";
-
-class App extends React.Component {
-  // state={warehouses: []}
-  state = { warehouses: [], init: 0 };
-
-  async componentDidMount() {
-    await axios.get("warehouses").then((res) => {
-      const warehouses = res.data;
-      this.setState({ warehouses: warehouses, init: 1 });
-    });
-    console.log(this.state.warehouses);
-  }
-
-  render() {
-    const { warehouses } = this.props;
->>>>>>> development
     return (
       <div className="App">
         {/* <DeleteWarehouse /> */}
@@ -55,20 +35,8 @@ class App extends React.Component {
         {/* <Background /> */}
         <BrowserRouter>
           <Switch>
-<<<<<<< HEAD
             <Route exact path="/" component={WarehouseList}/>
             <Route path="/:id" component={WarehouseDetails}/>
-=======
-            {/* <WarehouseList warehouses={this.state.warehouses}/> */}
-            <Route
-              exact
-              path="/"
-              render={(props) => (
-                <WarehouseList {...props} warehouses={warehouses} />
-              )}
-            />
-            {/* <Route path="/warehouses/:id" warehouses={this.state.warehouses.warehouseInfo} inventory={this.state.inventory} component={WarehouseDetails}/> */}
->>>>>>> development
             {/* <Route path="/editwarehouse" component={EditWarehouse} /> */}
             {/* <Route path="/addwarehouse" component={AddNewWarehouse} /> */}
             {/* <Route path="/inventories" component={InventoryList} /> */}
