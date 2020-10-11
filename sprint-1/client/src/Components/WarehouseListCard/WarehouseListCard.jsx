509ed09952f69879b1e9788482ca4f31827db707
Zemen Teshome <zementeshome@gmail.com>
2020-10-11 16:45:50 -0400

    styling fixed warehouse details & inventory

--- sprint-1/client/src/Components/WarehouseListCard/WarehouseListCard.jsx
+++ sprint-1/client/src/Components/WarehouseListCard/WarehouseListCard.jsx
@@ -2,38 +2,38 @@ import React from "react";
 
 class WarehouseListCard extends React.Component {
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return (
       <section className="warehouse">
         <div className="warehouse__content-container">
           <div className="warehouse__left-container">
             <h3 className="warehouse__sub-heading">WAREHOUSE</h3>
-            <p className="warehouse__name">{this.props.name}</p>
-            <img
-              className="warehouse__arrow"
-              src={
-                process.env.PUBLIC_URL + "/assets/Icons/chevronright24px.svg"
-              }
-              alt="right arrow"
-            />
-            <h3 className="warehouse__sub-heading">ADDRESS</h3>
-            <p className="warehouse__address">{this.props.address}</p>
-            <p className="warehouse__city">{this.props.city}</p>
-            <p className="warehouse__country">{this.props.country}</p>
+            <div className="warehouse__name-container">
+              <p className="warehouse__name">{this.props.name}</p>
+              <img className="warehouse__arrow" src={process.env.PUBLIC_URL + "/assets/Icons/chevronright24px.svg"}
+              alt="right arrow"/>
           </div>
-          <div className="warehouse__right-container">
+              <h3 className="warehouse__sub-heading">ADDRESS</h3>
+              <div className="warehouse__address-container">
+              <p className="warehouse__address">{this.props.address},</p>
+              <p className="warehouse__city">{this.props.addressCity},</p>
+              <p className="warehouse__country">{this.props.addressCountry}</p>
+          </div>
+          </div>
+            <div className="warehouse__right-container">
             <h3 className="warehouse__sub-heading">CONTACT NAME</h3>
-            <p className="warehouse__contact">{this.props.contact.name}</p>
+            <p className="warehouse__contact">{this.props.contact}</p>
             <h3 className="warehouse__sub-heading">CONTACT INFORMATION</h3>
             <p className="warehouse__contact-phone">
               {this.props.contactPhone}
             </p>
             <p className="warehouse__contact-email">
-              {this.props.contact.email}
+              {this.props.contactEmail}
             </p>
           </div>
         </div>
-        <img
+          <div className="warehouse__icon-container">
+          <img
           className="warehouse__delete-icon"
           src={process.env.PUBLIC_URL + "/assets/Icons/deleteoutline24px.svg"}
           alt="delete icon"
@@ -43,6 +43,7 @@ class WarehouseListCard extends React.Component {
           src={process.env.PUBLIC_URL + "/assets/Icons/edit24px.svg"}
           alt="edit icon"
         />
+        </div>
         <span className="warehouse__underline"></span>
       </section>
     );
